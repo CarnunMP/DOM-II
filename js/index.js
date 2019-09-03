ea31d3ff757f2108e313f7cdb4d407a154855812
CarnunMP <carnun@hotmail.co.uk>
2019-09-03 13:06:43 +0100

    mouseover and keydown used

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-// Your code goes here
+// Note: Very few of these event listeners do sensible or particularly interesting things! :P
+
+// Makes the text of the nav elements bold on mouseover:
+let anchorsArray = document.body.querySelectorAll("a");
+// console.log(anchorsArray);
+anchorsArray.forEach(a => {
+    a.addEventListener("mouseover", () => {
+        toggleBold(a);
+    });
+
+    function toggleBold(a) {
+        if (a.style.fontWeight != "bold") {
+            a.style.fontWeight = "bold";
+        } else {
+            a.style.fontWeight = "normal";
+        }
+    }
+});
+
+// Makes a space bar press scroll to the bottom of the page:
+window.addEventListener("keydown", (pressed) => {
+    if (pressed.key === " ") {
+        window.scrollTo(0,document.body.scrollHeight);
+    }
+});
