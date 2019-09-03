09fe524825db36d32238d837ee7052be78bb7141
CarnunMP <carnun@hotmail.co.uk>
2019-09-03 14:05:48 +0100

    wheel event added

--- js/index.js
+++ js/index.js
@@ -23,3 +23,26 @@ window.addEventListener("keydown", (pressed) => {
         window.scrollTo(0,document.body.scrollHeight);
     }
 });
+
+// Makes mouse wheel change the image opacity instead of scroll:
+window.addEventListener("wheel", (event) => {
+    event.preventDefault();
+
+    let images = document.querySelectorAll("img");
+    images.forEach(image => {
+        if (image.style.opacity === "") {
+            image.style.opacity = "1.0";
+        }
+        (event.deltaY > 0) ? decreaseOpacity(image) : increaseOpacity(image);
+    });
+
+    function decreaseOpacity(image) {
+        image.style.opacity = (parseFloat(image.style.opacity) * 0.9).toString();
+    }
+
+    function increaseOpacity(image) {
+        let imageOpacity = parseFloat(image.style.opacity);
+        imageOpacity = imageOpacity * 1.1;
+        image.style.opacity = Math.min(1, imageOpacity).toString();
+    }
+}, {passive: false}); // (Chrome ignores .preventDefault() otherwise, in this case.)
